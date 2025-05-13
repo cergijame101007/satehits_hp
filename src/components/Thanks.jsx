4b2718198834d4058502da1ee3cd341a5ab5672c
cergijame101007 <ginnojames247@gmail.com>
2025-05-14 00:45:04 +0900

    add style at thanks

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import './Thanks.css'
+
 const Thanks = () => {
     return (
-        <section className="thanks-wrapper">
+        <section className='thanks-wrapper'>
             <h2>送信ありがとうございました！</h2>
             <p>飲食店オーナーがご予約内容を確認し、折り返しご入力いただいたメールアドレスへ折り返しご連絡いたします。</p>
-            <p></p>
+            <p>ご入力いただいたメールアドレス宛に確認メールをお送りしていますが、迷惑メールフォルダに振り分けられてしまう場合があります。</p>
+            <p>お手数ですが、メールフォルダにて確認できない場合は迷惑メールフォルダもあわせてご確認ください。</p>
+            <Link to='/' className='back-link'>トップへ戻る</Link>
         </section>
     )
 }
