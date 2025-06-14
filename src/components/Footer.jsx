6de66e60949299f81988d2c63a7ac3bc775cdaf7
cergijame101007 <ginnojames247@gmail.com>
2025-06-14 15:58:21 +0900

    change instagram url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <p>© 2025 さて羊に戻るとしよう - All Rights Reserved</p>
-            <a href="https://www.instagram.com/sate_hits/" target="_blank" class="instagram-link">
+            <a href="https://www.instagram.com/satehits/" target="_blank" class="instagram-link">
                 <img src="./Instagram_Glyph_White.svg" alt="Instagram" class="instagram-icon" />
             </a>
         </footer>
