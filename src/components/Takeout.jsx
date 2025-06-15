749ea2e77d10e903813109df410141ec2603e968
cergijame101007 <ginnojames247@gmail.com>
2025-06-15 11:36:00 +0900

    change menu and styles

--- src/components/Takeout.jsx
+++ src/components/Takeout.jsx
@@ -4,9 +4,9 @@ const Takeout = () => {
     return (
         <section id="takeout" className="takeout">
             <h2>テイクアウト</h2>
-            <p>和紅茶 (HOT/ICE) ー ￥500(税込み)</p>
-            <p>米粉のクラフティ ー ￥300(税込み)</p>
-            <p>和紅茶の小売りパック ー 店舗内にて表示</p>
+            <p>和紅茶 (HOT/ICE) ー ￥500/￥550</p>
+            <p>米粉のクラフティ ー ￥300</p>
+            <p>和紅茶の小売りパック ー 20g ￥1200～/5g ￥300～</p>
             <div>和紅茶の小売りパックについてはオンライン販売もしております。詳しくはこちらの<a href="https://satehits.stores.jp/">STORESボタン</a>から</div>
         </section >
     );
