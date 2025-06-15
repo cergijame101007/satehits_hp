7dac04bd33ed03559e94986239dc7966a910a3c9
cergijame101007 <ginnojames247@gmail.com>
2025-06-15 12:06:13 +0900

    adjust styles of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer>
             <p>© 2025 さて羊に戻るとしよう - All Rights Reserved</p>
-            <a href="https://www.instagram.com/satehits/" target="_blank" class="instagram-link">
-                <img src="./Instagram_Glyph_White.svg" alt="Instagram" class="instagram-icon" />
+            <a href="https://www.instagram.com/satehits/" target="_blank" className="instagram-link">
+                <img src="./Instagram_Glyph_White.svg" alt="Instagram" className="instagram-icon" />
             </a>
         </footer>
     );
